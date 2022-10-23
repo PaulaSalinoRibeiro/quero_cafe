fc67673d2cd56b0dfd951dbd872ad4a8905899ab
Paula <paulasalinor@gmail.com>
2022-10-22 21:18:36 -0300

    feat: add addProduct mutation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,5 +1,12 @@
 const Product = require('../model/Product');
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLList, GraphQLSchema } = require('graphql');
+const { 
+  GraphQLObjectType, 
+  GraphQLID, 
+  GraphQLString, 
+  GraphQLList, 
+  GraphQLSchema,
+  GraphQLNonNull,
+ } = require('graphql');
 
 const ProductType = new GraphQLObjectType({
   name: 'Product',
@@ -28,12 +35,43 @@ const RootQuery = new GraphQLObjectType({
       type: ProductType,
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return Product.findOne({ id: args.id })
+        return Product.findById(args.id)
       }
     }
   }
 })
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addProduct: {
+      type: ProductType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        flavor: { type: GraphQLNonNull(GraphQLString) },
+        type: { type: GraphQLNonNull(GraphQLString) },
+        grind: { type: GraphQLNonNull(GraphQLString) },
+        description: { type: GraphQLNonNull(GraphQLString) },
+        price: { type: GraphQLNonNull(GraphQLString) },
+        image: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const product = new Product({ 
+          name: args.name, 
+          flavor: args.flavor,
+          type: args.type,
+          grind: args.grind,
+          description: args.description,
+          price: args.price,
+          image: args.image
+        })
+        return product.save()
+      }
+    },
+  }
+})
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation
 })
